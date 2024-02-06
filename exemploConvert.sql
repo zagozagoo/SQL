828f9f6d41ea24989e26f8e1c8f71ca22d9bc499
Zago! <144025196+zagozagoo@users.noreply.github.com>
2024-02-06 13:41:37 -0300

    Create exemploConvert.sql

---
+++ exemploConvert.sql
@@ -0,0 +1,3 @@
+select Convert(varchar(10), getdate(), 103) as DataSelect --data formatada bonita 01/02/2024, 103 é codigo padrao da microsoft pra datas no brasil
+
+select Convert(varchar(5), getdate(),108) as Horario --esse pra horario formatado 16:52

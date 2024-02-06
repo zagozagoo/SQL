fc874e8546eb32dc69a9b2607a5a5058843395f8
Zago! <144025196+zagozagoo@users.noreply.github.com>
2024-02-06 14:15:45 -0300

    Update exercicio4.sql

--- exercicio4.sql
+++ exercicio4.sql
@@ -8,6 +8,6 @@ from EventoPessoa as EP
 	RIGHT JOIN Pessoa as P -- Nesse caso tem que usar RIGHT, pois algumas pessoas não participaram mas precisam estar na tabela
 	on EP.IDPessoa = P.IDPessoa
 
-GROUP BY P.Nome -- nesse caso não tem f[ormula então pode usar P.Nome ao invés de "Nomes"
+GROUP BY P.Nome -- nesse caso não tem fórmula então pode usar P.Nome ao invés de "Nomes"
 
 ORDER BY Participação DESC-- como no "participação eu usei a formula COUNT, só pode usar o apelido "participação", não o EP.IDPessoa

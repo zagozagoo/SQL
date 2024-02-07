3d1ed299a3a7b8c6bd1ced58b666e96b95e13405
Zago! <144025196+zagozagoo@users.noreply.github.com>
2024-02-07 16:50:44 -0300

    Update desafio.sql

--- desafio.sql
+++ desafio.sql
@@ -1,1 +1,20 @@
+--Crie uma função cujo o retorno deverá ser um único select e retornar uma tabela com:
+--1- Quantidade de pessoas presentes no primeiro evento (mais antigo) que ocorreu.
+--2- Nome da sala em que esse evento ocorreu.
+--3- A Função será chamada com um parâmetro de busca. Como Aula, Palestra ou Reunião.
+--Nome Evento, Nome Sala, Qtd Pessoas, Data Início, Hora Início.
 
+select TOP 1 --vai usar o mais antigo
+	E.Descricao as Evento,
+	S.Nome as "Nome da Sala",
+	COUNT(EP.IDPessoa) as Participação,
+	Convert(varchar(10), E.DtHrInicio, 103) as "Data de Início",
+	Convert(varchar(5), getdate(),108) as "Horário de Início"
+	
+from Evento as E
+	INNER JOIN Sala as S
+	on E.IDSala = S.IDSala
+	INNER JOIN EventoPessoa as EP
+	on E.IDEvento = EP.IDEvento
+
+ORDER BY DtHrInicio

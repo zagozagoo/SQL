2b549140277503ff28a68ccd539d6ce5406c4b12
Zago! <144025196+zagozagoo@users.noreply.github.com>
2024-02-08 13:42:47 -0300

    Update DesafiocomFUNCAO.sql

--- DesafiocomFUNCAO.sql
+++ DesafiocomFUNCAO.sql
@@ -1,3 +1,8 @@
+--Crie uma função cujo o retorno deverá ser um único select e retornar uma tabela com:
+--1- Quantidade de pessoas presentes no primeiro evento (mais antigo) que ocorreu.
+--2- Nome da sala em que esse evento ocorreu.
+--3- A Função será chamada com um parâmetro de busca. Como Aula, Palestra ou Reunião.
+--Nome Evento, Nome Sala, Qtd Pessoas, Data Início, Hora Início.
 
 CREATE function fEventoAntigo (@busca VARCHAR(10))
 RETURNS TABLE AS
